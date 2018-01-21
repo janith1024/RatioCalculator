d7fdf5c5b710b4ab3af79894ba8b2be95d5805a2
Janith Sharada Amarasingha <janith1024@gmail.com>
2018-01-21 14:48:07 +0530

    add method to get excel value for the name

--- src/main/java/com/js/util/ExcelManager.java
+++ src/main/java/com/js/util/ExcelManager.java
@@ -2,10 +2,13 @@ package com.js.util;
 
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.*;
 
 import java.io.*;
+import java.util.Iterator;
 
 public class ExcelManager {
 
@@ -28,9 +31,15 @@ public class ExcelManager {
       this.sheet = sheet;
     }
 
-    public double getDouble(int raw,int col){
+    /**
+     * This method return the double value of the excel sheet for the given row and the column
+     * @param row row of the excel file
+     * @param col column of the file
+     * @return double value
+     */
+    public double getDouble(int row,int col){
       if(sheet != null){
-        XSSFRow sheetRow = sheet.getRow(raw);
+        XSSFRow sheetRow = sheet.getRow(row);
         if(sheetRow != null){
           XSSFCell cell = sheetRow.getCell(col);
           if(cell != null) {
@@ -39,6 +48,30 @@ public class ExcelManager {
         }
       }
       return 0;
+    }
+
+    /**
+     * This method is return double value of the given column for the matching name of the first column
+     * @param name name of field
+     * @param col column of the excel
+     * @return double value
+     */
+    public double getDouble(String name,int col){
+      if(sheet != null){
+        Iterator<Row> loop = sheet.iterator();
+        while (loop.hasNext()){
+          Row row = loop.next();
+          if(row.getCell(0).getStringCellValue().equalsIgnoreCase(name)){
+            Cell cell = row.getCell(col);
+            if(cell != null) {
+              return cell.getNumericCellValue();
+            }
+            break;
+          }
+        }
+      }
+      return 0;
+
     }
   }
 }
